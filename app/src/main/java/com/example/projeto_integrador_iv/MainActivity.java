18b1f3663ce61b69abb2a7559974499c26d51699
Gustavo Moreira <gustavo.ms@protonmail.com>
2021-05-09 15:31:18 -0300

    Add/TelaInicial

--- app/src/main/java/com/example/projeto_integrador_iv/MainActivity.java
+++ app/src/main/java/com/example/projeto_integrador_iv/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.projeto_integrador_iv;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
