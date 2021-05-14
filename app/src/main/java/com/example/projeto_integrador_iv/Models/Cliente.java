01d8fa3e161af803f30134069369c6d9ea05c91f
Alison Marques <55607652+AlisonMarques@users.noreply.github.com>
2021-05-14 19:11:32 -0300

    update model cliente (#20)

--- app/src/main/java/com/example/projeto_integrador_iv/Models/Cliente.java
+++ app/src/main/java/com/example/projeto_integrador_iv/Models/Cliente.java
@@ -1,9 +1,10 @@
 package com.example.projeto_integrador_iv.Models;
 
+public class Cliente {
 import java.io.Serializable;
 
 public class Cliente implements Serializable {
-    private String cpf;
+    private String cpf_cliente;
     private String nome;
     private String telefone;
     private String email;
@@ -13,20 +14,20 @@ public class Cliente implements Serializable {
 
     }
 
-    public Cliente(String cpf, String nome, String telefone, String email, String uf) {
-        this.cpf = cpf;
+    public Cliente(String cpf_cliente, String nome, String telefone, String email, String uf) {
+        this.cpf_cliente = cpf_cliente;
         this.nome = nome;
         this.telefone = telefone;
         this.email = email;
         this.uf = uf;
     }
 
-    public String getCpf() {
-        return cpf;
+    public String getCpfCliente() {
+        return cpf_cliente;
     }
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
+    public void setCpfCliente(String cpf_cliente) {
+        this.cpf_cliente = cpf_cliente;
     }
 
     public String getNome() {
@@ -63,7 +64,7 @@ public class Cliente implements Serializable {
 
     @Override
     public String toString() {
-        return "CPF: " + getCpf() + "\n" + " Nome: " + getNome() + "\n " +
+        return "CPF: " + getCpfCliente() + "\n" + " Nome: " + getNome() + "\n " +
                 "Fone: " + getTelefone() + "\n " + "E-mail: " + getEmail() + "\n " +
                 "UF: " + getUf();
     }
