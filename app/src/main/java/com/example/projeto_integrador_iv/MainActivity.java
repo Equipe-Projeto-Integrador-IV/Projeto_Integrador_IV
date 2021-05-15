fe1e6ac133ee484f3c06aa9f2a58870f57989aa1
Gustavo Moreira <gustavo.ms@protonmail.com>
2021-05-15 15:46:33 -0300

    adicionado no painel botões referente ao Serviço

--- app/src/main/java/com/example/projeto_integrador_iv/MainActivity.java
+++ app/src/main/java/com/example/projeto_integrador_iv/MainActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.projeto_integrador_iv.Telas.TelaCadastrarFuncionario;
+import com.example.projeto_integrador_iv.Telas.TelaLogin;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     //EM PRODUÇO, TESTANDO....
@@ -37,7 +40,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         else if (v==btnEntrar){
 
-            Intent telaEntrar = new Intent(this,TelaLogin.class);
+            Intent telaEntrar = new Intent(this, TelaLogin.class);
             startActivity(telaEntrar);
         }
 
