697ea4583e65fd93ed5f84d43a19f63913536bef
Gustavo Moreira <56530903+gutobyte@users.noreply.github.com>
2021-05-15 17:58:07 -0300

    mudanças em layout e criação de novas telas v_1 (#26)

--- app/src/main/java/com/example/projeto_integrador_iv/Telas/TelaPainelControle.java
+++ app/src/main/java/com/example/projeto_integrador_iv/Telas/TelaPainelControle.java
@@ -8,11 +8,16 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.projeto_integrador_iv.R;
+import com.example.projeto_integrador_iv.Telas.PainelControle.DeletarUsuario;
+import com.example.projeto_integrador_iv.Telas.PainelControle.EditarUsuarioInput;
+import com.example.projeto_integrador_iv.Telas.PainelControle.TelaCadastroUsuario;
 
 public class TelaPainelControle extends AppCompatActivity implements View.OnClickListener{
 
     Button btn_PainCadUsu;
     Button btn_PainVoltar;
+    Button btn_PainEditUsu;
+    Button btn_PainExcluirUsu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,10 @@ public class TelaPainelControle extends AppCompatActivity implements View.OnClic
         btn_PainCadUsu.setOnClickListener(this);
         btn_PainVoltar = findViewById(R.id.btnPainelVoltar);
         btn_PainVoltar.setOnClickListener(this);
+        btn_PainEditUsu = findViewById(R.id.btnPainelEditarCliente);
+        btn_PainEditUsu.setOnClickListener(this);
+        btn_PainExcluirUsu = findViewById(R.id.btnPainelExcluirCliente);
+        btn_PainExcluirUsu.setOnClickListener(this);
     }
 
     @Override
@@ -33,9 +42,17 @@ public class TelaPainelControle extends AppCompatActivity implements View.OnClic
             Intent tela = new Intent(this, TelaLogin.class);
             startActivity(tela);
         }
-        if(v==btn_PainCadUsu){
+        else if(v==btn_PainCadUsu){
             Intent tela2 = new Intent(this, TelaCadastroUsuario.class);
             startActivity(tela2);
+        }
+        else if(v==btn_PainEditUsu){
+            Intent tela3 = new Intent(this, EditarUsuarioInput.class);
+            startActivity(tela3);
+        }
+        else if(v==btn_PainExcluirUsu){
+            Intent tela4 = new Intent(this, DeletarUsuario.class);
+            startActivity(tela4);
         }
 
     }
