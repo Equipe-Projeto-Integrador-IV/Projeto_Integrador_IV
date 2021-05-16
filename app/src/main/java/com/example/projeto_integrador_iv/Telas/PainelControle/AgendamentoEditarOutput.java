6a611fa966c6c4a6b668178033bfa3150581f9c9
Gustavo Moreira <56530903+gutobyte@users.noreply.github.com>
2021-05-16 20:41:00 -0300

    ajustes v2 (#28)

---
+++ app/src/main/java/com/example/projeto_integrador_iv/Telas/PainelControle/AgendamentoEditarOutput.java
@@ -0,0 +1,15 @@
+package com.example.projeto_integrador_iv.Telas.PainelControle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.projeto_integrador_iv.R;
+
+public class AgendamentoEditarOutput extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agendamento_editar_output);
+    }
+}
