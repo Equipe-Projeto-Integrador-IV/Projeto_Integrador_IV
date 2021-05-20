eb0bb449ddb0028ad08ceb643b4cd920deaee729
AlisonMarques <alison-marques@outlook.com.br>
2021-05-19 21:14:37 -0300

    Desenvolvido a tela de listar clientes

--- app/src/main/java/com/example/projeto_integrador_iv/Telas/PainelControle/painelUsuario/DeletarUsuario.java
+++ app/src/main/java/com/example/projeto_integrador_iv/Telas/PainelControle/painelUsuario/DeletarUsuario.java
@@ -3,7 +3,7 @@ package com.example.projeto_integrador_iv.Telas.PainelControle.painelUsuario;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.projeto_integrador_iv.R;
 import com.example.projeto_integrador_iv.Telas.TelaPainelControle;
-import com.example.projeto_integrador_iv.dao.ConexaoDAO;
+import com.example.projeto_integrador_iv.dao.ClienteDAO;
 import com.example.projeto_integrador_iv.models.Cliente;
 
 
@@ -18,7 +18,7 @@ public class DeletarUsuario extends AppCompatActivity implements View.OnClickLis
     Button btnDELETAR_excluir;
     Button btnDELETAR_voltar;
     EditText edtDELETE;
-    ConexaoDAO Cdao;
+    ClienteDAO Cdao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +45,7 @@ public class DeletarUsuario extends AppCompatActivity implements View.OnClickLis
         }
         else if (v==btnDELETAR_excluir){
 
-            Cdao = new ConexaoDAO(this);
+            Cdao = new ClienteDAO(this);
             Cliente c = new Cliente();
             c.setCpfCliente(edtDELETE.getText().toString());
             Cdao.excluir(c);
