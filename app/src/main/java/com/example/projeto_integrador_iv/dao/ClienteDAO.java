eb9545e7b38e397545674ba5651e59134b514924
AlisonMarques <alison-marques@outlook.com.br>
2021-05-19 21:56:03 -0300

    Desenvolvido o crud do cliente

--- app/src/main/java/com/example/projeto_integrador_iv/dao/ClienteDAO.java
+++ app/src/main/java/com/example/projeto_integrador_iv/dao/ClienteDAO.java
@@ -1,83 +1,102 @@
-package com.example.projeto_integrador_iv.dao;
+    package com.example.projeto_integrador_iv.dao;
 
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+    import android.content.ContentValues;
+    import android.content.Context;
+    import android.database.Cursor;
+    import android.database.sqlite.SQLiteDatabase;
 
-import com.example.projeto_integrador_iv.models.Cliente;
+    import com.example.projeto_integrador_iv.models.Cliente;
 
-import java.util.ArrayList;
-import java.util.List;
+    import java.util.ArrayList;
+    import java.util.List;
 
-public class ClienteDAO {
-
-
-    private final String TABELA = "cliente";
-
-    private final String[] CAMPOS = {"cpf_cliente, nome, telefone, email, uf"};
+    public class ClienteDAO implements Dao<Cliente> {
 
 
-    private SQLiteDatabase banco;
-    private Conexao conexao;
+        private final String TABELA = "cliente";
 
-    public ClienteDAO(Context context){
-
-        conexao = new Conexao(context);
-        banco = conexao.getWritableDatabase();
-    }
+        private final String[] CAMPOS = {"cpf_cliente, nome, telefone, email, uf"};
 
 
-    //DAO REFERENTE AO CLIENTE
+        private SQLiteDatabase banco;
+        private Conexao conexao;
 
-    //preencher do cliente
-    private ContentValues preencherValoresCliente(Cliente cliente){
+        public ClienteDAO(Context context){
 
-        ContentValues values = new ContentValues();
-
-        values.put("cpf_cliente", cliente.getCpfCliente());
-        values.put("nome", cliente.getNome());
-        values.put("telefone", cliente.getTelefone());
-        values.put("email", cliente.getEmail());
-        values.put("uf", cliente.getUf());
-
-        return values;
-    }
-
-    //inserir cliente no banco
-    public long inserirCliente(Cliente cliente){
-
-        ContentValues values = preencherValoresCliente(cliente);
-
-        return banco.insert(TABELA, null, values);
-    }
-
-
-
-    //excluir cliente no banco
-    public long excluir(Cliente cliente){
-
-        return banco.delete(TABELA,"cpf_cliente=?", new String[]{cliente.getCpfCliente()});
-
-    }
-
-    public List<Cliente>  listar(){
-        Cursor c = banco.query(TABELA, CAMPOS, null, null, null, null, null);
-
-        List<Cliente> lista = new ArrayList<>();
-        while(c.moveToNext()){
-            Cliente cliente = new Cliente();
-            cliente.setCpfCliente(c.getString(0));
-            cliente.setNome(c.getString(1));
-            cliente.setTelefone(c.getString(2));
-            cliente.setEmail(c.getString(3));
-            cliente.setUf(c.getString(4));
-            lista.add(cliente);
-
+            conexao = new Conexao(context);
+            banco = conexao.getWritableDatabase();
         }
-        return lista;
+
+        //DAO REFERENTE AO CLIENTE
+
+        //preencher do cliente
+        private ContentValues preencherValoresCliente(Cliente cliente){
+
+            ContentValues values = new ContentValues();
+
+            values.put("cpf_cliente", cliente.getCpfCliente());
+            values.put("nome", cliente.getNome());
+            values.put("telefone", cliente.getTelefone());
+            values.put("email", cliente.getEmail());
+            values.put("uf", cliente.getUf());
+
+            return values;
+        }
+
+        //inserir cliente no banco
+        @Override
+        public long insert(Cliente cliente){
+
+            ContentValues values = preencherValoresCliente(cliente);
+
+            return banco.insert(TABELA, null, values);
+        }
+
+        @Override
+        public long update(Cliente cliente) {
+            ContentValues values = preencherValoresCliente(cliente);
+            return banco.update(TABELA, values, "cpf_cliente = ?", new String[]{String.valueOf(cliente.getCpfCliente())});
+        }
+
+        @Override
+        public List<Cliente> list() {
+            Cursor c = banco.query(TABELA, CAMPOS, null, null, null, null, null);
+
+            List<Cliente> lista = new ArrayList<>();
+            while(c.moveToNext()){
+                Cliente cliente = new Cliente();
+                cliente.setCpfCliente(c.getString(0));
+                cliente.setNome(c.getString(1));
+                cliente.setTelefone(c.getString(2));
+                cliente.setEmail(c.getString(3));
+                cliente.setUf(c.getString(4));
+                lista.add(cliente);
+
+            }
+            return lista;
+        }
+
+        @Override
+        public long remove(Cliente cliente) {
+            return banco.delete(TABELA,"cpf_cliente = ?", new String[]{cliente.getCpfCliente()});
+        }
+
+        @Override
+        public Cliente get(String cpf) {
+            Cursor c = banco.query(TABELA, CAMPOS,
+                    "id = ?", new String[] {String.valueOf(cpf)}, null, null, null);
+            if (c.moveToNext()) {
+                Cliente cliente = new Cliente();
+                cliente.setCpfCliente(c.getString(0));
+                cliente.setNome(c.getString(1));
+                cliente.setTelefone(c.getString(2));
+                cliente.setEmail(c.getString(3));
+                cliente.setUf(c.getString(4));
+                return cliente;
+            }
+            else {
+                return null;
+            }
+        }
+
     }
-
-
-
-}
