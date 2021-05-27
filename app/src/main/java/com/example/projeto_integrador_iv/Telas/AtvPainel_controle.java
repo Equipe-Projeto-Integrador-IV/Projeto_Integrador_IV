f4c4c23b3721306a72f961719459fd1c020f8840
Alison Marques <55607652+AlisonMarques@users.noreply.github.com>
2021-05-27 19:50:24 -0300

    update (#38)

---
+++ app/src/main/java/com/example/projeto_integrador_iv/Telas/AtvPainel_controle.java
@@ -0,0 +1,16 @@
+package com.example.projeto_integrador_iv.Telas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.projeto_integrador_iv.R;
+
+public class AtvPainel_controle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atv_painel_controle);
+    }
+}
